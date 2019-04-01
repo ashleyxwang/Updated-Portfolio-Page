1c6515730cab01171ccae47ec31d066997239a98
ashleyxwang <ashleywang514@yahoo.com>
2019-03-31 22:19:06 -0500

    debug modal

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,38 +1,41 @@
+let slideIndex = 1;
+let modalName = '#magazine';
+
 // Open Modal Functions
-function openModal(projects) {
-	$(projects).css('display', 'block');
+function openModal(modalId) {
+	slideIndex = 1;
+	modalName = modalId;
+	showSlides(slideIndex, modalId);
+	console.log(modalId);
+	$(modalId).css('display', 'block');
 }
 
 // Close the Modal X
-function closeModal(projects) {
-	$(projects).css('display', 'none');
+function closeModal(modalName) {
+	$(modalName).css('display', 'none');
 }
-
-var slideIndex = 1;
-showSlides(slideIndex);
-document.getElementById
 
 // Next/previous controls
 function plusSlides(n) {
-	showSlides(slideIndex += n);
+	showSlides(slideIndex += n, modalName);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+	showSlides(slideIndex = n, modalName);
 }
 
-function showSlides(n) {
-	var slides = document.getElementsByClassName("slides");
-	var dots = $('.thumbnail')
-	var captionText = document.getElementById("caption");
+function showSlides(n, modalId) {
+	let slides = document.querySelectorAll(modalId + " .slides");
+	let dots = $(modalId + ' .thumbnail')
+	let captionText = document.getElementById("caption");
 	if (n > slides.length) { slideIndex = 1 }
 	if (n < 1) { slideIndex = slides.length }
 
-	for (i = 0; i < slides.length; i++) {
+	for (let i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
-	for (i = 0; i < dots.length; i++) {
+	for (let i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
 	slides[slideIndex - 1].style.display = "block";
